9b7855b5404ee519659e1312fba047479d1b207a
Abhishek Kumar <154538311+abhi231210003@users.noreply.github.com>
2024-07-03 15:01:32 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -28757,24 +28757,24 @@ const imageAltText = "desktop with books and laptop";
  * links section. Below is a sample, update to reflect links you'd like to highlight.
  */ const projectList = [
     {
-        title: "10 Things To Know About Azure Static Web Apps \uD83C\uDF89",
-        description: "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-        url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i"
+        title: "Currency Converter",
+        description: "This project is a currency converter built using HTML, CSS, JavaScript, and an API to fetch currency codes and flags for respective countries. Features: Convert between different currencies using real-time exchange rates. View flags and currency codes for each country.",
+        url: "https://github.com/abhi231210003/CureencyConverter"
     },
     {
-        title: "Web Development for Beginners",
-        description: "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-        url: "https://github.com/microsoft/web-dev-for-beginners"
+        title: "Rock-paper-scissors",
+        description: "This is Rock-paper-scissors game made by me to sharpen my skills in frontend development.",
+        url: "https://github.com/abhi231210003/Rock-paper-scissors"
     },
     {
-        title: "My Resume Site",
-        description: "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-        url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website"
+        title: "Tic-Tac-Toe-Game",
+        description: "This is Tic-Tac-Toe game made by me in JavaScript to sharpen my skills.",
+        url: "https://github.com/abhi231210003/Tic-Tac-Toe-Game"
     },
     {
-        title: "GitHub Codespaces and github.dev",
-        description: "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-        url: "https://www.youtube.com/watch?v=c3hHhRME_XI"
+        title: "Tic-Tac-Toe-using-c",
+        description: "This is Tic-Tac-Toe game made by me in c language to sharpen my skills.",
+        url: "https://github.com/abhi231210003/Tic-Tac-Toe-using-c"
     }, 
 ];
 const Portfolio = ()=>{
